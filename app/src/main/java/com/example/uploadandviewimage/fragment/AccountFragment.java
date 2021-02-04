d12658e9736f1ad10d926d1b61ea5ae46fea1da9
narumiya1 <Manunited222222>
2021-02-04 14:52:37 +0700

    back up n' update account

--- app/src/main/java/com/example/uploadandviewimage/fragment/AccountFragment.java
+++ app/src/main/java/com/example/uploadandviewimage/fragment/AccountFragment.java
@@ -1,7 +1,9 @@
 package com.example.uploadandviewimage.fragment;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.se.omapi.Session;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,6 +29,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -40,6 +43,7 @@ public class AccountFragment extends Fragment {
     String getEmail, getAlamatz, getUsernamez,id, getId, getPw, getPwRetype;
     String getPphoneNumbbr;
     Sesion session;
+    Accounts accounts = new Accounts();
     public AccountFragment() {
     }
 
@@ -60,16 +64,20 @@ public class AccountFragment extends Fragment {
         session = new Sesion(getContext());
 //        txtData.setText(mAuth.getCurrentUser().getPhoneNumber());
         // logout
+        getPphoneNumbbr = session.getPhone();
         btnLogout = rootView.findViewById(R.id.btn_lougout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                FirebaseAuth.getInstance().signOut();
+                FirebaseAuth.getInstance().signOut();
 //                Intent intent = new Intent(getContext(), LoginActivity.class);
 //                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 //
 //                startActivity(intent);
                 session.logoutUser();
+//                Preference.clearLoggedInUser(getContext());
+//                startActivity(new Intent(getContext(),LoginActivity.class));
+//                session.logoutUser();
 
             }
         });
@@ -84,40 +92,84 @@ public class AccountFragment extends Fragment {
             }
         });
         // get Data from Insert using firebase data snapshot
-        rootDb = FirebaseDatabase.getInstance().getReference().child("accounts");
-        rootDb.addValueEventListener(new ValueEventListener() {
+//        rootDb = FirebaseDatabase.getInstance().getReference().child("User");
+//        rootDb.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                List<Accounts> bpfragmentTableList = new ArrayList<>();
+//
+//                for (DataSnapshot dataSnapshot1 : snapshot.getChildren()) {
+//
+//                    Accounts bpfragmentTable = dataSnapshot1.getValue(Accounts.class);
+////                    id = mAuth.getCurrentUser().getUid();
+//                    getId = bpfragmentTable.getPhone();
+//                    String getPhone;
+//                    getPhone = bpfragmentTable.getPhone();
+//                    //try 11 1 21
+//                    if (getPhone.equals(getId)) {
+//                        getUsernamez = bpfragmentTable.getUsername();
+//                        getEmail = bpfragmentTable.getEmail();
+//                        getAlamatz = bpfragmentTable.getAddress();
+//                        getPw = bpfragmentTable.getPassword();
+////                        getPwRetype = bpfragmentTable.getRetypePassword();
+//                        getPphoneNumbbr = bpfragmentTable.getPhone();
+//
+//
+//                        System.out.println(" founded" + getId + " " + id);
+//                        txtUsername.setText(getUsernamez);
+//                        txtAlamat.setText(getAlamatz);
+//                        textEmail.setText(getEmail);
+//                        txtData.setText(getPphoneNumbbr);
+//                        break;
+//                    }
+//                    bpfragmentTableList.add(bpfragmentTable);
+//
+//
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Sesion session = new Sesion(getActivity());
+        Query query = reference.child("User").child(session.getPhone());
+        query.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                List<Accounts> bpfragmentTableList = new ArrayList<>();
-
-                for (DataSnapshot dataSnapshot1 : snapshot.getChildren()) {
-
-                    Accounts bpfragmentTable = dataSnapshot1.getValue(Accounts.class);
-//                    id = mAuth.getCurrentUser().getUid();
-                    getId = bpfragmentTable.getPhoneNumbers();
-                    String getPhone;
-                    getPhone = bpfragmentTable.getPhoneNumbers();
-                    //try 11 1 21
-                    if (getPhone.equals(getId)) {
-                        getUsernamez = bpfragmentTable.getNama();
-                        getEmail = bpfragmentTable.getEmail();
-                        getAlamatz = bpfragmentTable.getAlamat();
-                        getPw = bpfragmentTable.getPassword();
-                        getPwRetype = bpfragmentTable.getRetypePassword();
-                        getPphoneNumbbr = bpfragmentTable.getPhoneNumbers();
-
-
-                        System.out.println(" founded" + getId + " " + id);
-                        txtUsername.setText(getUsernamez);
-                        txtAlamat.setText(getAlamatz);
-                        textEmail.setText(getEmail);
-                        txtData.setText(getPphoneNumbbr);
-                        break;
-                    }
-                    bpfragmentTableList.add(bpfragmentTable);
-
-
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Log.d("DATA CHANGEt", "onDataChange: "+dataSnapshot.getValue());
+                    accounts = dataSnapshot.getValue(Accounts.class);
+                    Log.d("DATA CHANGE -- ", "onDataChange: "+accounts.getAddress());
+                    txtUsername.setText(accounts.getUsername());
+                    txtAlamat.setText(accounts.getAddress());
+                    textEmail.setText(accounts.getEmail());
+                    txtData.setText(session.getPhone());
+//                    for (DataSnapshot issue : dataSnapshot.getChildren()) {
+//                        // do with your result
+//                        String name = dataSnapshot.child("Username").getValue(String.class);
+//                        Log.d(name,"output");
+//                        txtUsername.setText(name);
+//                        Accounts bpfragmentTable = issue.getValue(Accounts.class);
+////
+//                        getUsernamez = bpfragmentTable.getUsername();
+//                        getEmail = bpfragmentTable.getEmail();
+//                        getAlamatz = bpfragmentTable.getAddress();
+//                        getPw = bpfragmentTable.getPassword();
+////                        getPwRetype = bpfragmentTable.getRetypePassword();
+//                        getPphoneNumbbr = bpfragmentTable.getPhone();
+//                        String id = bpfragmentTable.getId();
+//                        getId = bpfragmentTable.getId();
+//                        System.out.println(" founded" + getId + " " + id);
+//
+////                        bpfragmentTableList.add(bpfragmentTable);
+//
+//
+//                    }
                 }
             }
 
@@ -126,21 +178,63 @@ public class AccountFragment extends Fragment {
 
             }
         });
-
-
+//        query.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//                    Log.d("DATA CHANGEt", "onDataChange: "+dataSnapshot.getValue());
+//                    Accounts accounts = dataSnapshot.getValue(Accounts.class);
+//                    Log.d("DATA CHANGEt", "onDataChange: "+accounts.getAddress());
+//                    txtUsername.setText(accounts.getUsername());
+//                    txtAlamat.setText(accounts.getAddress());
+//                    textEmail.setText(accounts.getEmail());
+//                    txtData.setText(accounts.getPhone());
+////                    for (DataSnapshot issue : dataSnapshot.getChildren()) {
+////                        // do with your result
+////                        String name = dataSnapshot.child("Username").getValue(String.class);
+////                        Log.d(name,"output");
+////                        txtUsername.setText(name);
+////                        Accounts bpfragmentTable = issue.getValue(Accounts.class);
+//////
+////                        getUsernamez = bpfragmentTable.getUsername();
+////                        getEmail = bpfragmentTable.getEmail();
+////                        getAlamatz = bpfragmentTable.getAddress();
+////                        getPw = bpfragmentTable.getPassword();
+//////                        getPwRetype = bpfragmentTable.getRetypePassword();
+////                        getPphoneNumbbr = bpfragmentTable.getPhone();
+////                        String id = bpfragmentTable.getId();
+////                        getId = bpfragmentTable.getId();
+////                        System.out.println(" founded" + getId + " " + id);
+////
+//////                        bpfragmentTableList.add(bpfragmentTable);
+////
+////
+////                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
         btn_updatedataacount = rootView.findViewById(R.id.btn_updatedataacount);
         btn_updatedataacount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent = new Intent(getContext(), AccountUpdateActivity.class);
-                intent.putExtra("id",id);
-                intent.putExtra("namez", getUsernamez);
-                intent.putExtra("email", getEmail);
-                intent.putExtra("alamat", getAlamatz);
-                intent.putExtra("phone", getPphoneNumbbr);
-                intent.putExtra("psswrd", getAlamatz);
-                intent.putExtra("psswrdretype", getAlamatz);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("user",accounts);
+                intent.putExtras(bundle);
+//                intent.putExtra("id",id);
+//                intent.putExtra("namez", accounts.getUsername());
+//                intent.putExtra("email", accounts.getEmail());
+//                intent.putExtra("alamat", accounts.getAddress());
+//                intent.putExtra("phone", accounts.getPhone());
+//                intent.putExtra("psswrd", accounts.getPassword());
+//                intent.putExtra("psswrdretype", accounts.getPassword());
+//                intent.putExtra("id", getId);
 
                 startActivity(intent);
 
